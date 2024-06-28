05870c99251ebccb92d1c3dd90ea0f09f0a52979
mauricio martinez <maumarcastil@gmail.com>
2024-06-28 00:48:37 -0500

    chore: Add flex class to body for layout styling improvement

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,7 +17,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${inter.className} min-h-screen p-4`}>{children}</body>
+      <body className={`${inter.className} flex min-h-screen p-4`}>
+        {children}
+      </body>
     </html>
   );
 }
