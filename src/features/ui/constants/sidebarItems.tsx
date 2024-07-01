cb936b9abe0f0557de2ea0f5fe8e954d8aa6e727
mauricio martinez <maumarcastil@gmail.com>
2024-07-01 14:46:20 -0500

    feat: Add sidebar navigation items to dashboard

---
+++ src/features/ui/constants/sidebarItems.tsx
@@ -0,0 +1,55 @@
+import {
+  FiCreditCard,
+  FiDollarSign,
+  FiEye,
+  FiHome,
+  FiMapPin,
+  FiTag,
+  FiTruck,
+  FiUser,
+} from 'react-icons/fi';
+
+import { SidebarItem } from '../types/sidebar';
+
+export const SIDEBAR_ITEMS: SidebarItem[] = [
+  {
+    title: 'General',
+    path: '/dashboard',
+    icon: <FiHome />,
+  },
+  {
+    title: 'Recolecciones',
+    path: '/dashboard/collections',
+    icon: <FiTruck />,
+  },
+  {
+    title: 'Generadores',
+    path: '/dashboard/generators',
+    icon: <FiUser />,
+  },
+  {
+    title: 'Retiros',
+    path: '/dashboard/withdrawals',
+    icon: <FiCreditCard />,
+  },
+  {
+    title: 'Mapa',
+    path: '/dashboard/map',
+    icon: <FiMapPin />,
+  },
+  {
+    title: 'Precio estándar',
+    path: '/dashboard/standard-price',
+    icon: <FiDollarSign />,
+  },
+  {
+    title: 'Verificación',
+    path: '/dashboard/verification',
+    icon: <FiEye />,
+  },
+  {
+    title: 'Transacciones',
+    path: '/dashboard/transactions',
+    icon: <FiTag />,
+  },
+];
