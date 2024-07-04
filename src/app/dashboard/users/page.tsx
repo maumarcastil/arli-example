a2dffb5460b0f6d33435d6ccb784a0cc99b7e6ea
mauricio martinez <maumarcastil@gmail.com>
2024-07-04 13:57:58 -0500

    feat: Create UsersPage component for displaying users

---
+++ src/app/dashboard/users/page.tsx
@@ -0,0 +1,15 @@
+import { Typography } from '@/features/ui/utils/materialTailwindExports';
+
+const UsersPage = () => {
+  return (
+    <>
+      <div>
+        <Typography variant="h2" className="text-3xl font-medium">
+          UsersPage
+        </Typography>
+      </div>
+    </>
+  );
+};
+
+export default UsersPage;
