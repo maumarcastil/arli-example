5aa5e1e4c8ccbf04ec082471a9f7aebc3ef0e883
mauricio martinez <maumarcastil@gmail.com>
2024-07-04 13:29:18 -0500

    feat: Add EditGeneratorForm component for editing generators

---
+++ src/features/generator/components/editGeneratorForm.tsx
@@ -0,0 +1,139 @@
+'use client';
+
+import React, { useEffect } from 'react';
+
+import {
+  Button,
+  Drawer,
+  IconButton,
+  Input,
+  Typography,
+} from '@material-tailwind/react';
+import { useFormik } from 'formik';
+
+import { cn } from '@/features/ui/utils';
+
+import { GeneratorDocument } from '@/features/generator/models/generator';
+
+interface CreateGeneratorFormProps {
+  isOpenDrawer: boolean;
+  onCloseDrawer: () => void;
+  selectedGenerator: GeneratorDocument | null;
+}
+
+export const EditGeneratorForm = (props: CreateGeneratorFormProps) => {
+  const { isOpenDrawer, onCloseDrawer, selectedGenerator } = props;
+
+  useEffect(() => {
+    if (isOpenDrawer) {
+      document.body.style.overflow = 'hidden';
+    } else {
+      document.body.style.overflow = '';
+    }
+
+    return () => {
+      document.body.style.overflow = '';
+    };
+  }, [isOpenDrawer]);
+
+  useEffect(() => {
+    formik.setValues({
+      name: selectedGenerator?.name || '',
+      email: selectedGenerator?.email || '',
+      comercialName: selectedGenerator?.comercialName || '',
+    });
+  }, [selectedGenerator]);
+
+  const formik = useFormik({
+    initialValues: {
+      name: selectedGenerator?.name || '',
+      email: selectedGenerator?.email || '',
+      comercialName: selectedGenerator?.comercialName || '',
+    },
+    onSubmit: (values) => {
+      console.log('🚀 ~ EditGeneratorForm ~ values:', values);
+    },
+  });
+
+  return (
+    <>
+      <Drawer open={isOpenDrawer} onClose={onCloseDrawer} placement="right">
+        <div className="flex h-full flex-col">
+          <div className="flex items-center justify-between border-b border-gray-200 px-4 py-2">
+            <Typography variant="h5" color="blue-gray">
+              Editar Generador
+            </Typography>
+            <IconButton
+              variant="text"
+              color="blue-gray"
+              onClick={onCloseDrawer}
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={2}
+                stroke="currentColor"
+                className="h-5 w-5"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M6 18L18 6M6 6l12 12"
+                />
+              </svg>
+            </IconButton>
+          </div>
+          <div className="flex-1 overflow-y-auto p-4">
+            <Typography
+              variant="small"
+              color="gray"
+              className="mb-5 font-normal"
+            >
+              Actualiza la información del generador. Todos los campos son
+              obligatorios.
+            </Typography>
+            <form
+              onSubmit={formik.handleSubmit}
+              className="flex flex-col gap-6"
+            >
+              <Input
+                type="text"
+                name="name"
+                label="Nombre"
+                crossOrigin={undefined}
+                value={formik.values.name}
+                onChange={formik.handleChange}
+                required
+              />
+              <Input
+                type="email"
+                name="email"
+                label="Correo Electrónico"
+                crossOrigin={undefined}
+                value={formik.values.email}
+                onChange={formik.handleChange}
+                required
+              />
+              <Input
+                type="text"
+                name="comercialName"
+                label="Nombre Comercial"
+                crossOrigin={undefined}
+                value={formik.values.comercialName}
+                onChange={formik.handleChange}
+                required
+              />
+              <Button
+                type="submit"
+                className={cn('rounded-2xl bg-[--bgSoft] text-inherit')}
+              >
+                Guardar
+              </Button>
+            </form>
+          </div>
+        </div>
+      </Drawer>
+    </>
+  );
+};
